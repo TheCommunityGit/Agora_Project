8210e92f7fa3d0ad5ff714500dd41c07f0e8b6ad
JuanchoDluffy <66972166+JuanchoDluffy@users.noreply.github.com>
2024-09-07 21:29:23 -0600

    created components directory and began work on main Nav bar

---
+++ frontend/src/components/nav.tsx
@@ -0,0 +1,19 @@
+// main navegation bar.
+function NavBar() {
+  return (
+    <nav id="main-nav-bar">
+      <div className="logo">Pete’s Trading Post</div>
+      <div>
+        <ul className="nav-links">
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+export default NavBar;
